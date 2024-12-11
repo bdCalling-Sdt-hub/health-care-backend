66d47c27951381a5e9c673c9b9d11e2a57022748
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-12-11 10:34:25 +0600

    fixing user controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -40,10 +40,9 @@ const updateProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = req.user;
     let profile;
-    if (req.files && 'image' in req.files && req.files.image[0]) {
-      profile = `/images/${req.files.image[0].filename}`;
+    if (req.files && 'profile' in req.files && req.files.profile[0]) {
+      profile = `/images/${req.files.profile[0].filename}`;
     }
-
     const data = {
       profile,
       ...req.body,
