db1f4c6da826b2c9f16e93f504737fa6740bb3d5
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-06 15:05:21 +0600

    fixing some issues

--- src/app/modules/consultation/consultation.service.ts
+++ src/app/modules/consultation/consultation.service.ts
@@ -433,7 +433,7 @@ const buyMedicine = async (userId: string, id: string) => {
             name: 'Consultation service Medicins.',
             description: 'Prescription medicins',
           },
-          unit_amount: Math.round(isExistConsultation.totalAmount + 20 * 100),
+          unit_amount: (Number(allMedicinsPrice) + 20) * 100,
         },
         quantity: 1,
       },
