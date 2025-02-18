0347a53117f4c8f083233269777369bac354ed00
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-02-18 14:41:54 +0600

    fixing more issues

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -68,7 +68,9 @@ const loginUserFromDB = async (payload: ILoginData) => {
 
 //forget password
 const forgetPasswordToDB = async (email: string) => {
-  const isExistUser = await User.isExistUserByEmail(email);
+  const isExistUser = await User.findOne({ email, verified: true }).select(
+    '+authentication'
+  );
   if (!isExistUser) {
     throw new ApiError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
   }
